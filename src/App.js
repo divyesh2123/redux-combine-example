2fd6bc12cae557addac234e9cb3347a1706b0793
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-01-05 09:40:33 +0530

    changes for app and useraction and postreducer and store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { ERO, REQUEST, SUC } from './action/userAction';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+
+  const dis = useDispatch();
+
+  const data= useSelector(y=>y.user);
+
+  console.log(data);
+
+  useEffect(()=>{
+
+    dis(REQUEST())
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(y=>y.json())
+    .then(y=>{
+
+      dis( SUC(y))
+    }).catch(y=> {
+
+
+      dis(ERO(y))
+    })
+
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
